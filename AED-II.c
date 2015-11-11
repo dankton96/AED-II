7cad7769888d3bc63f771201b951261310f4ec5d
dankton96 <dankton96@hotmail.com>
2015-11-11 19:46:26 -0200

    Update AED-II.c

--- AED-II.c
+++ AED-II.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "lista.h"
-#include "fila.h"
+#include <conio.h>
+//#include "/home/danton/Downloads/lista.h"
+//#include "/home/danton/Downloads/fila.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 int menu(int option)
@@ -17,16 +18,30 @@ int menu(int option)
 return option;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////
-
+char mover (char direction)
+{
+    direction=getch();
+    return direction;
+}
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
     int op;
-    while(op!=2)
+    char s;
+    /*while(op!=2)
     {
         int op;
         menu(op);
+        if(op==1)
+        {
+            //comeca o jogo
+        }*/
+    s=getch();
+    if(s==37){printf("esquerda\n");}
+    if(s==38){printf("cima\n");}
+    if(s==39){printf("direita\n");}
+    if(s==40){printf("baixo\n");}
+    else{printf("nao e um cursor\n");}
+    getch();
     };
-
-}
