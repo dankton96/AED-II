9cc49f03453a19bb8616588c45d2b5eeea14d1ce
dankton96 <dankton96@hotmail.com>
2015-11-22 17:54:34 -0200

    Create fogefoge.h

---
+++ fogefoge.h
@@ -0,0 +1,33 @@
+
+#ifndef _FOGEFOGE_H_
+#define _FOGEFOGE_H_
+
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+#define BOMBA 'b'
+
+int acabou()
+{
+    if()
+};
+void Move(char direcao)
+{
+  if(direcao==ESQUERDA){}
+}
+
+int ehdirecao(char direcao)
+{
+return
+		direcao == ESQUERDA ||
+		direcao == CIMA ||
+		direcao == BAIXO ||
+		direcao == DIREITA;
+};
+void fantasmas();
+void explodepilula();
+void explodepilula2(int x, int y, int somax, int somay, int qtd);
+
+
+#endif
